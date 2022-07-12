0382058d340f6a32ec6a51ba3e73e6d6f835dcf9
Santiago <r00k13d3v@gmail.com>
2022-07-12 19:53:40 +0200

    feat: add new image

--- service/server.js
+++ service/server.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send(`Your Node.js service is running on port ${PORT}...`);
+  res.send(`Your Node.js service is running on port ${PORT}... try with these path /hello`)	
 });
 
-app.get('/greeting/:communityID', (req, res) => {
-  res.send(`Hey ${req.params.communityID}, really nice seeing you here! 🙂`);
+app.get("/hello", (req, res) => {
+  res.send("Hello, Spocky! &#128406");
 });
 
 const PORT = process.env.PORT || 8080;
